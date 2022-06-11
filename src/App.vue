c8900d369b066decb09ef7efd4ac36a8b810e84f
ceyhan bekiroglu <ceyhanbekiroglu@gmail.com>
2022-06-11 23:48:47 +0200

    Fix `removeTodo` fuction

--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,8 @@ const addTodo = () => {
 	})
 }
 
-const removeTodo = todo => {
-	todos.value = todos.value.filter(t => t !== todo)
+const removeTodo = (todo) => {
+	todos.value = todos.value.filter((t) => t !== todo)
 }
 
 watch(todos, (newVal) => {
@@ -91,7 +91,7 @@ onMounted(() => {
 						<input type="text" v-model="todo.content">
 					</div>
 					<div class="actions">
-						<button class="delete" @click="removeTodo(todos)">Delete</button>
+						<button class="delete" @click="removeTodo(todo)">Delete</button>
 					</div>
 
 				</div>
